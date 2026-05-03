a98148d23815f6fa0fdcf65bfa37b6873c0585d1
akairatov1999-cmyk <akairatov1998@mail.ru>
2026-05-03 21:32:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ input_data = pd.get_dummies(input_data)
 columns = joblib.load('columns.joblib')
 input_data = input_data.reindex(columns=columns, fill_value=0)
 prediction = model.predict(input_data)[0]
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
 # Показываем предсказанную цену
 st.success(f"Предсказанная цена: {prediction:,.0f} тенге")
 
