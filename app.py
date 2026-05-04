b84cb59eb291473a717386164e3f41bf3cc3c773
akairatov1999-cmyk <akairatov1998@mail.ru>
2026-05-04 10:18:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -145,8 +145,7 @@ if st.button("Рекомендация от помощник"):
     # Запрос к OpenAI
     response = client.chat.completions.create(
         model="gpt-5.4-mini",
-      temperature=0.3,  # детерминированные решения
-        messages=[
+            messages=[
             {
                 "role": "user",
                 "content": prompt
